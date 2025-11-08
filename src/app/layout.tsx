ff03998336d3ecad8057dcdfafaad1c7e4dea630
Sarthak-jasuja <sarthakjasuja@icloud.com>
2025-11-09 02:22:41 +0530

    Add Convex authentication and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/theme/provider";
 import { Toaster } from "sonner";
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ConvexAuthNextjsServerProvider>
+        <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -40,5 +42,7 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
+    </ConvexAuthNextjsServerProvider>
+    
   );
 }
